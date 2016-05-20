8b314e030c8776bcf5a4378322744f98d8f6bc5f
Houston Upgrade <houston@palantir.com>
2016-05-20 17:53:51 -0400

    updated data structures and shipper

--- shipper/demo_shipper.py
+++ shipper/demo_shipper.py
@@ -30,6 +30,15 @@ from conf.nodes import nodes
 import hashlib
 import time
 
+# Init Variables
+#-----------------------------------------------------------------------#
+params = {
+	'key': 'testobj1',
+        'value': 'testvalue1'
+        }
+
+# Classes
+#-----------------------------------------------------------------------#
 class Parcel(object):
 	def __init__(self, params):
 		super(Parcel, self).__init__()
@@ -42,27 +51,46 @@ class Parcel(object):
 class Shipment(object):
 	def __init__(self):
 		super(Shipment, self).__init__()
-		self.container = []
+		self.manifest = {}
 		self.id = hashlib.md5(str(time.time())).hexdigest()
 
 	def assign(self, parcel):
-		self.calculate_route(parcel)
+		self.sort(self.calculate_route(parcel))
 	
 	def calculate_route(self, parcel):
-		parcel = sherpa.packer(parcel)
+		return sherpa.packer(parcel)
+
+	def sort(self, parcel):
+		if parcel.region in self.manifest.keys():
+			self.manifest[parcel.region].append(parcel)
+		else:
+			self.manifest[parcel.region] = Container(nodes[parcel.region])
+			self.manifest[parcel.region].append(parcel)
+
+	def routes(self):
+		return self.manifest.keys()
+
+class Container(object):
+        def __init__(self, address):
+                super(Container, self).__init__()
+		self.container = []
+		self.address = address
+
+	def append(self, parcel):
 		self.container.append(parcel)
- 
-params = {
-	'key': 'testobj1',
-	'value': 'testvalue1'
-	}
-sherpa = Sherpa(routes=len(nodes))
-parcel = Parcel(params)	
-shipment = Shipment()
-shipment.assign(parcel)
 
-print shipment.__dict__
+	def unpack(self):
+		return self.container
 
-s = SocketClient()
-
-print 'Hello World'
+# Main
+#-----------------------------------------------------------------------#
+if __name__ == '__main__':
+	sherpa = Sherpa(routes=len(nodes))
+	parcel = Parcel(params)	
+	shipment = Shipment()
+	shipment.assign(parcel)
+	print shipment.routes()
+	s = SocketClient()
+	response = s.agent_query(shipment)
+	
+	print response
